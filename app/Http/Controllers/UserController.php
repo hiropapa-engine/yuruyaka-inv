14066760bc92dd3039288e981daf712a2c8c1845
Hiroyuki Yamamoto <admin@hiropapa-engine.com>
2020-11-06 14:35:34 +0900

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function showJibunSim() {
+        return view('jibun_sim');
+    }
+}
---
+++ resources/views/components/welcome.blade.php
@@ -0,0 +1,3 @@
+<div class="p-6">
+  <h2><a href="/jibun_sim">じぶん年金シミュレータ</a></h2>
+</div>
